47836abcfeda0c2a0ae77fa85de0642c7c802ab1
Dayan Bravo <dayan3847@gmail.com>
2022-11-15 13:58:14 -0600

    feat: add example tree

--- src/Tree.hs
+++ src/Tree.hs
@@ -2,3 +2,5 @@ module Tree where
 
 --Binary Tree structure
 data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq)
+
+myTree = Node 104 (Node 71 (Node 17 (Node 3 Empty Empty) (Node 18 Empty Empty)) (Node 91 Empty Empty)) (Node 240 (Node 108 Empty (Node 110 Empty Empty)) (Node 245 Empty Empty))
